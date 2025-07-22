b30e3ebdae583fbd33b3b4b6944597a78e51353d
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-07-22 13:32:10 +0900

    Create 1695.java

---
+++ 2025-July/1695.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int ans = 0;
+        int fin = nums.length;
+        Queue<Integer> q = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+        int cur = 0;
+        for (int i = 0; i < fin; i++) {
+            int nu = nums[i];
+            while (set.contains(nu)) {
+                int a = q.poll();
+                cur -= a;
+                set.remove(a);
+            }
+            cur += nu;
+            q.add(nu);
+            set.add(nu);
+            ans = Math.max(ans, cur);
+        }
+        return ans;
+    }
+}
