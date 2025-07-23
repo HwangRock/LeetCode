69d2dd79518205af4081b9b5358402fd8cf9443e
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-07-23 19:31:02 +0900

    상태 변수와 마지막 문자에 따라 정답 문자열에 넣을지 말지 결정

--- 2025-July/1957.java
+++ 2025-July/1957.java
@@ -1,23 +1,21 @@
 class Solution {
     public String makeFancyString(String s) {
-        StringBuilder sb=new StringBuilder();
-        Boolean state=false;
+        StringBuilder sb = new StringBuilder();
+        Boolean state = false;
 
-        int fin=s.length();
-        for(int i=0;i<fin;i++){
-            char c=s.charAt(i);
+        int fin = s.length();
+        for (int i = 0; i < fin; i++) {
+            char c = s.charAt(i);
 
-            int sbl=sb.length();
-            if(state==true && c==sb.charAt(sbl - 1)){
+            int sbl = sb.length();
+            if (state == true && c == sb.charAt(sbl - 1)) {
                 continue;
-            }
-            else if(sbl>0 && c==sb.charAt(sbl - 1)){
-                state=true;
+            } else if (sbl > 0 && c == sb.charAt(sbl - 1)) {
+                state = true;
                 sb.append(c);
-            }
-            else{
+            } else {
                 sb.append(c);
-                state=false;
+                state = false;
             }
         }
 
