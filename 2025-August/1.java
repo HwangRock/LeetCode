684081446b9438d4ffbbceaa5449d1b191785009
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-08-23 23:48:23 +0900

    map에 현재와 목표의 차이가 있는지 확인하며 탐색

---
+++ 2025-August/1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int fin = nums.length;
+        for (int i = 0; i < fin; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
